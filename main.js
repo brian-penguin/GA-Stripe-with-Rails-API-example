f4b8692fd2e304ee9cc364a22a8c0b3ae1ef623a
brimatteng <btenggren20@gmail.com>
2014-11-12 15:37:52 -0500

    Add a create token call back handler

--- main.js
+++ main.js
@@ -14,19 +14,43 @@ RAILS API
 $(document).ready(function() {
     console.log( "ready!" );
 
-    Stripe.setPublishableKey('YOUR_PUBLISHABLE_KEY');
+    // Set the Stripe Key on Doc load
+    Stripe.setPublishableKey('pk_test_BTBy2xVMMlqrvoRZr1DRY8W5');
 
+  // form submit handler
   $('#payment-form').submit(function(event) {
     var $form = $(this);
 
     // Disable the submit button to prevent repeated clicks
     $form.find('button').prop('disabled', true);
 
+    // Create a token for the card -> send to response handler
     Stripe.card.createToken($form, stripeResponseHandler);
 
     // Prevent the form from submitting with the default action
     return false;
   });
+
+  // Token creator call back function
+  function stripeResponseHandler(status, response) {
+    var $form = $('#payment-form');
+
+    if (response.error) {
+      // Show the errors on the form
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('button').prop('disabled', false);
+    }
+    else {
+      // response contains id and card, which contains additional card details
+      var token = response.id;
+      console.log(token);
+      // Insert the token into the form so it gets submitted to the server
+      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      // and submit
+      $form.get(0).submit();
+    }
+  }
+
 });
 
 
